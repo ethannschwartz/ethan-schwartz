aee2ebb125abfa0e2eb878d40ba0afcf2f3e1472
ethannschwartz <96845694+ethannschwartz@users.noreply.github.com>
2024-07-31 08:43:53 +0300

    Added icons, but hid them from html

--- components/skills.vue
+++ components/skills.vue
@@ -6,7 +6,8 @@
         <h5 class="mb-4 font-bold text-3xl type-secondary">{{ category.title }}</h5>
         <ul>
           <li data-aos="fade-up" class="flex items-center gap-2 type-primary" v-for="skill in category.skills">
-            <i class="text-xs" :class="skill.logo"></i>
+            <!--            <i class="text-base" :class="skill.logo"></i>-->
+                        <i class="text-sm fi fi-rr-check"></i>
             {{ skill.name }}
           </li>
         </ul>
@@ -32,23 +33,23 @@ const skillsArray = [
   {
     title: "Frameworks and Libraries",
     skills: [
-      { name: "Vue.js", logo: "fi fi-rr-social-network" },
-      { name: "Nuxt.js", logo: "fi fi-rr-social-network" },
-      { name: "Angular", logo: "fi fi-rr-social-network" },
-      { name: "Laravel", logo: "fi fi-rr-social-network" },
-      { name: "Inertia.js", logo: "fi fi-rr-social-network" },
-      { name: "Redux", logo: "fi fi-rr-social-network" },
-      { name: "NgRx / NgXs / RxJS", logo: "fi fi-rr-social-network" },
-      { name: "TailwindCSS", logo: "fi fi-rr-social-network" },
+      { name: "Vue.js", logo: "fi fi-rr-check" },
+      { name: "Nuxt.js", logo: "fi fi-rr-check" },
+      { name: "Angular", logo: "fi fi-rr-check" },
+      { name: "Laravel", logo: "fi fi-rr-check" },
+      { name: "Inertia.js", logo: "fi fi-rr-check" },
+      { name: "Redux", logo: "fi fi-rr-check" },
+      { name: "NgRx / NgXs / RxJS", logo: "fi fi-rr-check" },
+      { name: "TailwindCSS", logo: "fi fi-rr-check" },
       { name: "Node.js", logo: "fi fi-brands-node-js" }
     ]
   },
   {
     title: "Backend Technologies",
     skills: [
-      { name: ".NET Entity Framework", logo: "fi fi-rr-social-network" },
+      { name: ".NET Entity Framework", logo: "fi fi-rr-check" },
       { name: "REST APIs", logo: "fi fi-rr-api" },
-      { name: "AWS", logo: "fi fi-rr-social-network" },
+      { name: "AWS", logo: "fi fi-rr-check" },
       { name: "CI/CD", logo: "fi fi-rr-network-cloud" }
     ]
   },
@@ -56,27 +57,27 @@ const skillsArray = [
     title: "Tools and Platforms",
     skills: [
       { name: "Github", logo: "fi fi-brands-github" },
-      { name: "Jira / Atlassian", logo: "fi fi-rr-social-network" },
+      { name: "Jira / Atlassian", logo: "fi fi-rr-check" },
       { name: "Figma", logo: "fi fi-brands-figma" },
-      { name: "Cloudinary", logo: "fi fi-rr-social-network" },
+      { name: "Cloudinary", logo: "fi fi-rr-check" },
       { name: "SEO", logo: "fi fi-rr-discover" },
       { name: "Google Search Console", logo: "fi fi-brands-google" },
-      { name: "OpenAI GPT", logo: "fi fi-rr-social-network" },
+      { name: "OpenAI GPT", logo: "fi fi-rr-check" },
       { name: "Stripe", logo: "fi fi-brands-stripe" }
     ]
   },
   {
     title: "Development Techniques and Practices",
     skills: [
-      { name: "MVC Architecture", logo: "fi fi-rr-social-network" },
-      { name: "SPA", logo: "fi fi-rr-social-network" },
-      { name: "Agile Development", logo: "fi fi-rr-social-network" },
-      { name: "Asynchronous functions", logo: "fi fi-rr-social-network" },
-      { name: "UX/UI Design", logo: "fi fi-rr-social-network.png" },
-      { name: "Real-time data fetching", logo: "fi fi-rr-social-network" },
-      { name: "Performance optimization", logo: "fi fi-rr-social-network" },
-      { name: "Technical SEO", logo: "fi fi-rr-social-network.png" },
-      { name: "Dynamic content handling", logo: "fi fi-rr-social-network" }
+      { name: "MVC Architecture", logo: "fi fi-rr-check" },
+      { name: "SPA", logo: "fi fi-rr-check" },
+      { name: "Agile Development", logo: "fi fi-rr-check" },
+      { name: "Asynchronous functions", logo: "fi fi-rr-check" },
+      { name: "UX/UI Design", logo: "fi fi-rr-check.png" },
+      { name: "Real-time data fetching", logo: "fi fi-rr-check" },
+      { name: "Performance optimization", logo: "fi fi-rr-check" },
+      { name: "Technical SEO", logo: "fi fi-rr-check.png" },
+      { name: "Dynamic content handling", logo: "fi fi-rr-check" }
     ]
   },
   {
